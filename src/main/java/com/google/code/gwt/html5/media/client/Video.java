a202939d10411ce11cca43b7b6a3062e25f7b6c2
Michael Scharfstein <smike@google.com>
2010-08-20 15:10:07 -0400

    Adding ability to specify pre-existing Element in Media Widgets, also fixing media events so that they don't get removed when new event handlers get registered

--- src/main/java/com/google/code/gwt/html5/media/client/Video.java
+++ src/main/java/com/google/code/gwt/html5/media/client/Video.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Mark Renouf
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHDIR
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -21,7 +21,7 @@ package com.google.code.gwt.html5.media.client;
 public class Video extends Media {
   /**
    * Determine if it's possible to play back the given video type
-   * 
+   *
    * @param type The media type to test
    * @return [ "" | "maybe" | "probably" ]
    */
@@ -30,12 +30,16 @@ public class Video extends Media {
   }
 
   public Video() {
-    setElement(VideoElement.create());
+    this(VideoElement.create());
   }
 
   public Video(String src) {
     this();
     setSrc(src);
+  }
+
+  public Video(VideoElement element) {
+    super(element);
   }
 
   public native int getVideoWidth() /*-{
